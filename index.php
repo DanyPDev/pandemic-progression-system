6037c17466a4fb31d61308502d58546c9ba51ea4
Dany Pham <danyencs@gmail.com>
2022-07-30 22:01:55 -0400

    add bootstrap

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <title>COMP353 Project</title>
     <link rel="stylesheet" href="style.css">
+    <!-- CSS only -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
 	<script src="index.js"></script>
