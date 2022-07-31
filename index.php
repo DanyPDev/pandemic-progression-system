73948cc57acdf460b8d9ef65476b869289525c5f
Dany Pham <danyencs@gmail.com>
2022-07-30 22:40:42 -0400

    nav bar

--- index.php
+++ index.php
@@ -4,11 +4,39 @@
     <meta charset="UTF-8">
     <title>COMP353 Project</title>
     <link rel="stylesheet" href="style.css">
-    <!-- CSS only -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
+   <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+
+</head>
+
   <body>
-	<script src="index.js"></script>
-    <h1>test</h1>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">COVID SYSTEM</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarScroll">
+                <div>
+                  <form class="d-flex">
+                    <input class="form-control me-2" type="username" name="username" placeholder="Username" aria-label="Search">
+                    <input class="form-control me-2" type="password" name="password" placeholder="Password" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Login</button>
+                  </form>
+                </div>
+
+                    <a class="navbar-nav ml-auto me-auto my-2 navbar-nav-scroll nav-link active" aria-current="page" href="#">Home</a>
+                 
+                <form class="d-flex">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>
   </body>
+
 </html>
